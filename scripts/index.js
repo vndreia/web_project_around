e6503f1c3cd5ba54481549cb3298af2cb72dfe96
Andrea <andreapalacios481@gmail.com>
2025-04-30 12:30:58 -0600

    correction 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,45 @@
 //Popup ---> 1. Select DOM elements
-const popupButton = document.querySelector('.action-bar__button-type-form');
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__button-close');
+const popupButton = document.querySelector(".main-bar__button-type-form");
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__button-close");
 
 // ---> 2. Manipulate elements
 function openPopUp() {
-    popup.classList.add('popup_opened');  //Adds the class to popup, not the button
+  popup.classList.add("popup_opened"); //Adds the class to popup, not the button
 }
 
-popupButton.addEventListener('click', openPopUp); //Adds the event to the button, not the class
+popupButton.addEventListener("click", openPopUp); //Adds the event to the button, not the class
 
 function closePopUp() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
 // ---> 3. Create events
-popupClose.addEventListener('click', closePopUp);
-
+popupClose.addEventListener("click", closePopUp);
 
 //Form
-const formElement = document.querySelector('.form');
-console.log(formElement); 
-const inputName = document.querySelector('.form__input-type-name');
-console.log(inputName);  
-const inputJob = document.querySelector('.form__input-type-about');
-console.log(inputJob); 
-const editName = document.querySelector('.action-bar__title');
+const formElement = document.querySelector(".form");
+console.log(formElement);
+const inputName = document.querySelector(".form__input-type-name");
+console.log(inputName);
+const inputJob = document.querySelector(".form__input-type-about");
+console.log(inputJob);
+const editName = document.querySelector(".main-bar__title");
 console.log(editName);
-const editJob = document.querySelector('.action-bar__paragraph');
+const editJob = document.querySelector(".main-bar__paragraph");
 console.log(editJob);
 //Manipulation
 function handleSubmit(evt) {
-    evt.preventDefault();  // Previene el comportamiento por defecto del formulario (que recargue la página)
-    
-    const name = inputName.value;   // Obtener el valor del campo 'name'
-    const job = inputJob.value;  // Obtener el valor del campo 'about'
-    
-    editName.textContent = name;  // Actualizar el nombre en la parte visible
-    editJob.textContent = job;  // Actualizar el 'about' en la parte visible
-    popup.classList.remove('popup_opened');
+  evt.preventDefault(); // Previene el comportamiento por defecto del formulario (que recargue la página)
+
+  const name = inputName.value; // Obtener el valor del campo 'name'
+  const job = inputJob.value; // Obtener el valor del campo 'about'
+
+  editName.textContent = name; // Actualizar el nombre en la parte visible
+  editJob.textContent = job; // Actualizar el 'about' en la parte visible
+  popup.classList.remove("popup_opened"); // Cerrar el popup después de enviar el formulario
 }
 
 //Eventos
-formElement.addEventListener('submit', handleSubmit);  // Aquí estamos escuchando el evento submit del formulario
+formElement.addEventListener("submit", handleSubmit); // Aquí estamos escuchando el evento submit del formulario
 console.log(editName, editJob);
