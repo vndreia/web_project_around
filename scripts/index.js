5f5752d693380c961fa5f811c17572d64968567c
Andrea <andreapalacios481@gmail.com>
2025-07-08 15:19:35 -0600

    add: initiate Cards in index

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,34 @@
 //Popup ---> 1. Select DOM elements
-import { openProfilePopup, openProfileButton, popupProfile, closeProfilePopup, closeProfileButton, popupAddPlace, openAddPopup, closeAddPopup, addButton, openImagePopup } from "./utils.js"; //Goes a the top and mixes functions and vars
+import {
+  openProfilePopup,
+  openProfileButton,
+  popupProfile,
+  closeProfilePopup,
+  closeProfileButton,
+  popupAddPlace,
+  openAddPopup,
+  closeAddPopup,
+  addButton,
+  initialCards,
+  cardTemplate,
+  cardsZone,
+} from "./utils.js"; //Goes a the top and mixes functions and vars
 import { enableValidation } from "./FormValidator.js"; //Importing the validation function
 import { Card } from "./cards.js";
 
 
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
-
-//AQUÍ QUÉ PASÓ?
-  initialCards.forEach((card) => {
-    const newCard = new Card(card, template)
-    const newNode = newCard._renderCard();
-    cardsZone.appendChild(card);
-  });
+//Instantiate is saved in a const
+initialCards.forEach((card) => {
+  const newCard = new Card(card, cardTemplate);
+  const newNode = newCard._renderCard();
+  cardsZone.appendChild(newNode); 
+});
 
 openProfileButton.addEventListener("click", openProfilePopup); //Adds the event to the button, not the class
 
 closeProfileButton.addEventListener("click", closeProfilePopup);
-document.addEventListener("keydown", (evt) => { //Remember evt.keys work only in the whole document, not in a specific element
+document.addEventListener("keydown", (evt) => {
+  //Remember evt.keys work only in the whole document, not in a specific element
   if (evt.key === "Escape") {
     closeProfilePopup();
   }
@@ -52,8 +39,7 @@ popupProfile.addEventListener("click", (evt) => {
   }
 });
 
-
-//Edit profile form 
+//Edit profile form
 const formElement = document.querySelector(".form");
 console.log(formElement);
 const inputName = document.querySelector(".form__input-type-name");
@@ -90,8 +76,25 @@ enableValidation({
   errorClass: "popup__error_visible",
 });
 
-
 //Add image popup
+function handleForm(evt) {
+  evt.preventDefault();
+  const name = addPlaceInput.value;
+  const link = addLinkInput.value;
+
+  if (!name || !link) {
+    alert("Por favor, completa los campos");
+    return;
+  }
+  const cardData = { name, link };
+  const card = createCard(cardData);
+  cardsZone.appendChild(card);
+
+  formAdd.reset(); // Limpia inputs
+  closePopup(); // Cierra el popup
+}
+
+formAdd.addEventListener("submit", handleForm);
 addButton.addEventListener("click", openAddPopup);
 
 buttonClose.addEventListener("click", closeAddPopup);
